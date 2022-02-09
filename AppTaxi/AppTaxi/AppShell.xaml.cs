1d6f7ec0cf96c54aacc81eda3cb3deebfc14ff8c
ElvisKocho <q.elviskocho12@gmail.com>
2022-02-09 10:43:46 -0500

    Primer cambio

---
+++ AppTaxi/AppTaxi/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppTaxi.ViewModels;
+using AppTaxi.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppTaxi
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
